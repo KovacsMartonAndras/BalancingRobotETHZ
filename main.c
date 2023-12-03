3434ec5fd3245a8979c370a68d10d85bc8997d68
KovacsMartonAndras <64225531+KovacsMartonAndras@users.noreply.github.com>
2023-12-03 21:51:31 +0100

    Update main.c
    convert percentages to duty cycle function added,
    Motor control

--- main.c
+++ main.c
@@ -24,7 +24,7 @@
 #include "lsm6dsl.h"
 #include "b_l475e_iot01a1_bus.h"
 #include <stdio.h>
-
+#include <math.h>
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -34,7 +34,6 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -43,43 +42,31 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
+TIM_HandleTypeDef htim2;
 UART_HandleTypeDef huart1;
-const double DELTA_TIME = 0.003846;
+
 /* USER CODE BEGIN PV */
 LSM6DSL_Object_t MotionSensor;
 volatile uint32_t dataRdyIntReceived;
-
-// Kalman filter parameters
-float process_variance = 0.9;     // Process noise variance
-float measurement_variance = 0.01;   // Measurement noise variance
-
-// Kalman filter state for x-axis
-float estimate_x = 0.0;
-float estimate_error_x = 0.5;
-
-// Kalman filter function for x-axis
-void kalman_filter_x(float accel_angle_x, float gyro_rate_x, float dt) {
-    // Prediction step
-    float prediction_x = estimate_x + gyro_rate_x * dt;
-    float prediction_error_x = estimate_error_x + process_variance;
-
-    // Update step
-    float kalman_gain_x = prediction_error_x / (prediction_error_x + measurement_variance);
-    estimate_x = prediction_x + kalman_gain_x * (accel_angle_x - prediction_x);
-    estimate_error_x = (1 - kalman_gain_x) * prediction_error_x;
-}
+volatile float DutyCycle;
+double DELTA_TIME = 0.003846;
+int ANGLE_TRESH_HOLD = 2;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
 void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_USART1_UART_Init(void);
+static void MX_TIM2_Init(void);
 /* USER CODE BEGIN PFP */
 static void MEMS_Init(void);
+void Configure_DutyCycle(void);
+double convert_duty_cycle(float percent);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+
 //PRE: Acceleration values of y and z axis with int32_t type
 //POST: Angle with respect to z axis on the z-y plane
 double getAngleFromAcceleration(int32_t y, int32_t z)
@@ -87,8 +74,49 @@ double getAngleFromAcceleration(int32_t y, int32_t z)
 	return (atan((double) y / (double) z)*(-180.0/M_PI));
 }
 
+//PRE: Acceleration values of y and z axis with int32_t type
+//POST: Angle with respect to z axis on the z-y plane
+double getAngleFromGyro(int32_t gyroX)
+{
+	static double angleZY = 0.0;
+	static double prevGyroX = 0.0;
+	// High-pass filter to compensate for drift
+	double filteredGyroX = gyroX - prevGyroX;
 
+	angleZY += filteredGyroX * DELTA_TIME;
+	//printf("Angle: %2d \r\n",(int)angleZY);
+	prevGyroX = gyroX;
+	//TODO: consant integration can lead to exceeding the limits of double type -> Solve by resetting?
+	/*
+	if(angleZY > 200)
+	{
+		//Reset Variables
+		angleZY = 0.0;
+		prevGyroX = 0.0;
+	}
+	*/
+	return angleZY;
+}
 
+// Kalman filter parameters
+  float process_variance = 0.9;     // Process noise variance
+  float measurement_variance = 0.01;   // Measurement noise variance
+
+  // Kalman filter state for x-axis
+  float estimate_x = 0.0;
+  float estimate_error_x = 4;
+
+  // Kalman filter function for x-axis
+  void kalman_filter_x(float accel_angle_x, float gyro_rate_x, float dt) {
+      // Prediction step
+      float prediction_x = estimate_x + gyro_rate_x * dt;
+      float prediction_error_x = estimate_error_x + process_variance;
+
+      // Update step
+      float kalman_gain_x = prediction_error_x / (prediction_error_x + measurement_variance);
+      estimate_x = prediction_x + kalman_gain_x * (accel_angle_x - prediction_x);
+      estimate_error_x = (1 - kalman_gain_x) * prediction_error_x;
+  }
 /* USER CODE END 0 */
 
 /**
@@ -120,41 +148,65 @@ int main(void)
   /* Initialize all configured peripherals */
   MX_GPIO_Init();
   MX_USART1_UART_Init();
+  MX_TIM2_Init();
   /* USER CODE BEGIN 2 */
+  //Initialize Accelerometer Sensor
   dataRdyIntReceived = 0;
   MEMS_Init();
+  HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim2,TIM_CHANNEL_2);
+  //printf("UART WORKS");
+
+  //Define structs
   LSM6DSL_Axes_t acc_axes;
   LSM6DSL_Axes_t gyro_axes;
+  DutyCycle = 7.5; //1.5ms IDLE
+  //TODO: Correction for temperature difference
+  //uint32_t correctionX,correctionY,correctionZ = calibrateGyroscope();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
+while (1)
+{
+	if (dataRdyIntReceived != 0)
+	{
+	   dataRdyIntReceived = 0;
 
-  while (1)
-  {
-	  if (dataRdyIntReceived) {
-		  dataRdyIntReceived = 0;
+	   //Data Aquisition
+	   LSM6DSL_ACC_GetAxes(&MotionSensor, &acc_axes);
+	   LSM6DSL_GYRO_GetAxes(&MotionSensor, &gyro_axes);
 
-		  // in dps (degrees per second)
-		  LSM6DSL_GYRO_GetAxes(&MotionSensor, &gyro_axes);
-		  // in mg (milli g)
-		  LSM6DSL_ACC_GetAxes(&MotionSensor, &acc_axes);
+	   //Calculate angle from raw data
+	   double angleA = getAngleFromAcceleration(acc_axes.y, acc_axes.z);
+	   kalman_filter_x(angleA, gyro_axes.x, DELTA_TIME);
+	   printf("data: %5d\r\n", (int)estimate_x);
 
+	   //TODO: Function to set speed
+	   if((int) estimate_x < -ANGLE_TRESH_HOLD)
+	   {
+		// DutyCycle = 3.5; //0.7ms MAX Speed
+		   DutyCycle = convert_duty_cycle(-100);
+	   }
+	   else if((int) estimate_x > ANGLE_TRESH_HOLD)
+	   {
+		   // DutyCycle = 11.5; // 2.3ms MAX Speed
+		   DutyCycle = convert_duty_cycle(-100);
+	   }
+	   else
+	   {
+		   // DutyCycle = 7.5;
+		   DutyCycle = convert_duty_cycle(0);
+	   }
 
-		  //double angleA = getAngleFromGyro(gyro_axes.x);
-		  double angleA = getAngleFromAcceleration(acc_axes.y,acc_axes.z);
-		  kalman_filter_x(angleA, gyro_axes.x, DELTA_TIME);
-
-		  printf("angle: % 5d\r\n", (int) estimate_x);
-		  // printf("ACC: % 5d, % 5d, % 5d\r\n",  (int) acc_axes.x, (int) acc_axes.y, (int) acc_axes.z);
-		  //printf("GYRO: % 5d, % 5d, % 5d\r\n",  (int) gyro_axes.x, (int) gyro_axes.y, (int) gyro_axes.z);
+	   Configure_DutyCycle();
 	  }
+}
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-  }
-  /* USER CODE END 3 */
 }
+  /* USER CODE END 3 */
 
 /**
   * @brief System Clock Configuration
@@ -207,6 +259,69 @@ void SystemClock_Config(void)
 }
 
 /**
+  * @brief TIM2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM2_Init(void)
+{
+
+  /* USER CODE BEGIN TIM2_Init 0 */
+
+  /* USER CODE END TIM2_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+  TIM_OC_InitTypeDef sConfigOC = {0};
+
+  /* USER CODE BEGIN TIM2_Init 1 */
+
+  /* USER CODE END TIM2_Init 1 */
+  htim2.Instance = TIM2;
+  htim2.Init.Prescaler = 79; // (80-1)
+  htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim2.Init.Period = 20000;
+  htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_ENABLE;
+  if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim2, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_Init(&htim2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_UPDATE;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim2, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sConfigOC.OCMode = TIM_OCMODE_PWM1;
+  sConfigOC.Pulse = 0;
+  sConfigOC.OCPolarity = TIM_OCPOLARITY_HIGH;
+  sConfigOC.OCFastMode = TIM_OCFAST_DISABLE;
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_1) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_TIM_PWM_ConfigChannel(&htim2, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM2_Init 2 */
+
+  /* USER CODE END TIM2_Init 2 */
+  HAL_TIM_MspPostInit(&htim2);
+
+}
+
+/**
   * @brief USART1 Initialization Function
   * @param None
   * @retval None
@@ -248,26 +363,27 @@ static void MX_USART1_UART_Init(void)
   */
 static void MX_GPIO_Init(void)
 {
-	GPIO_InitTypeDef GPIO_InitStruct = {0};
-	/* USER CODE BEGIN MX_GPIO_Init_1 */
-	/* USER CODE END MX_GPIO_Init_1 */
+  GPIO_InitTypeDef GPIO_InitStruct = {0};
+/* USER CODE BEGIN MX_GPIO_Init_1 */
+/* USER CODE END MX_GPIO_Init_1 */
 
-	  /* GPIO Ports Clock Enable */
-	  __HAL_RCC_GPIOB_CLK_ENABLE();
-	  __HAL_RCC_GPIOD_CLK_ENABLE();
+  /* GPIO Ports Clock Enable */
+  __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
+  __HAL_RCC_GPIOD_CLK_ENABLE();
 
-	  /*Configure GPIO pin : PD11 */
-	  GPIO_InitStruct.Pin = GPIO_PIN_11;
-	  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
-	  GPIO_InitStruct.Pull = GPIO_NOPULL;
-	  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
+  /*Configure GPIO pin : PD11 */
+  GPIO_InitStruct.Pin = GPIO_PIN_11;
+  GPIO_InitStruct.Mode = GPIO_MODE_IT_RISING;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  HAL_GPIO_Init(GPIOD, &GPIO_InitStruct);
 
-	  /* EXTI interrupt init*/
-	  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
-	  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
+  /* EXTI interrupt init*/
+  HAL_NVIC_SetPriority(EXTI15_10_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(EXTI15_10_IRQn);
 
-	/* USER CODE BEGIN MX_GPIO_Init_2 */
-	/* USER CODE END MX_GPIO_Init_2 */
+/* USER CODE BEGIN MX_GPIO_Init_2 */
+/* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -315,15 +431,38 @@ static void MEMS_Init(void)
   LSM6DSL_GYRO_Enable(&MotionSensor);
 }
 
-void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
-	if (GPIO_Pin == GPIO_PIN_11) {
-		dataRdyIntReceived++;
-	}
+//Callback function for the interrupt
+void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin)
+{
+  if (GPIO_Pin == GPIO_PIN_11) {
+    dataRdyIntReceived++;
+  }
 }
 
-int _write(int fd, char * ptr, int len) {
-	HAL_UART_Transmit(&huart1, (uint8_t *) ptr, len, HAL_MAX_DELAY);
-	return len;
+//UART Redirection
+int _write(int fd, char * ptr, int len)
+{
+  HAL_UART_Transmit(&huart1, (uint8_t *) ptr, len, HAL_MAX_DELAY);
+  return len;
+}
+
+
+void Configure_DutyCycle(void) {
+
+	uint32_t T; /* PWM signal period */
+	uint32_t P; /* PWM Pulse duration */
+	T = __HAL_TIM_GET_AUTORELOAD(&htim2);
+	P = (T*DutyCycle)/100;
+	__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_1,P);
+	__HAL_TIM_SET_COMPARE(&htim2,TIM_CHANNEL_2,P);
+}
+
+double convert_duty_cycle(float percent)
+{
+	// percent 100 is max speed in one direction
+	// percent -100 is max speed in other direction
+	// percent 0 = STOP
+	return 7.5 + (percent/100)*4.0;
 }
 /* USER CODE END 4 */
 
@@ -335,11 +474,9 @@ void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
   /* User can add his own implementation to report the HAL error return state */
-  // __disable_irq();
+  __disable_irq();
   while (1)
   {
-	  HAL_GPIO_TogglePin(LED2_GPIO_Port, LED2_Pin);
-	  HAL_Delay(50);
   }
   /* USER CODE END Error_Handler_Debug */
 }
